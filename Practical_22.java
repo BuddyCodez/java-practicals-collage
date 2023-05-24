02bd846671ab9a45c52264fb27cdcb2b72ef47bb
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-05-24 16:31:22 +0530

    new update

---
+++ Practical_22.java
@@ -0,0 +1,8 @@
+package pack1;
+import pack2.demo;
+public class Practical_22 {
+    public static void main(String[] args) {
+        demo d1 = new demo();
+        d1.display();
+    }
+}
---
+++ demo.java
@@ -0,0 +1,7 @@
+package pack2;
+public class demo {
+    public void display() {
+        System.out.println("Udit Vegad(216090307001)");
+        System.out.println("This method is called from demo.java"); 
+    }
+}
